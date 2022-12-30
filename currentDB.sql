cd07ec41cfe8a96d7e4d4e1ba1c7066b03a84a62
singularity <12184989+singularity-s0@users.noreply.github.com>
2022-12-30 18:32:26 +0800

    update db

--- currentDB.sql
+++ currentDB.sql
@@ -66,7 +66,7 @@ ALTER SEQUENCE public.auth_user_id_seq OWNED BY public.auth_user.id;
 --
 
 CREATE TABLE public.movies (
-    movie_id integer,
+    movie_id integer NOT NULL,
     moviename character varying(255),
     starname character varying(255),
     detail character varying(255),
@@ -86,6 +86,28 @@ CREATE TABLE public.movies (
 ALTER TABLE public.movies OWNER TO postgres;
 
 --
+-- Name: movies_movie_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.movies_movie_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.movies_movie_id_seq OWNER TO postgres;
+
+--
+-- Name: movies_movie_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.movies_movie_id_seq OWNED BY public.movies.movie_id;
+
+
+--
 -- Name: auth_user id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -93,13 +115,19 @@ ALTER TABLE ONLY public.auth_user ALTER COLUMN id SET DEFAULT nextval('public.au
 
 
 --
+-- Name: movies movie_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies ALTER COLUMN movie_id SET DEFAULT nextval('public.movies_movie_id_seq'::regclass);
+
+
+--
 -- Data for Name: auth_user; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.auth_user (id, username, password, is_superuser, first_name, last_name, email, is_active, roal) FROM stdin;
-1	abc	qzxf99SNgPvFAskM$xb9mdCmyKX8DIFcSZl92t/HJG6i5KSdNbtnhFntUNu4=	f	a	a	aijeidj@dm.cs	t	\N
-2	abbbb	Nf76VvliexB9SExf$wp/9bDGookOag3BNISOLT4FT5FJu3WkZJdc2VQXRfpw=	f			sdsf@df.c	t	\N
-3	admin	7AU2i88zodv4RHIf$7Tqo5IfJGgh13woWdn+v1LpgZv3A+DttsLNzwvmZEvo=	f				t	admin
+1	admin	9yod80hbsscwIp7v$ncDRV6rkxLm9CfZE0+8+Pi5ZrsaV42powxsuQJ07qvo=	f			admin@zxj.com	t	admin
+2	zxj	r0LX03ExdhaFgy55$NBt4x38HXuDC3KmUiFpx0d1yb+613foVNRutzg/KmZI=	f			zxj@zxj.com	t	user
 \.
 
 
@@ -108,6 +136,7 @@ COPY public.auth_user (id, username, password, is_superuser, first_name, last_na
 --
 
 COPY public.movies (movie_id, moviename, starname, detail, running_time, type, avg_rating, poster, box_office, num_participants, release_date, box_office_unit, foreign_name, location) FROM stdin;
+1	Avatar	zxj	a new movie	20	sci-fi	0		0	0	0		avatar	shanghai
 \.
 
 
@@ -115,7 +144,14 @@ COPY public.movies (movie_id, moviename, starname, detail, running_time, type, a
 -- Name: auth_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.auth_user_id_seq', 3, true);
+SELECT pg_catalog.setval('public.auth_user_id_seq', 2, true);
+
+
+--
+-- Name: movies_movie_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.movies_movie_id_seq', 1, true);
 
 
 --
@@ -127,11 +163,11 @@ ALTER TABLE ONLY public.auth_user
 
 
 --
--- Name: auth_user auth_user_username_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: movies movies_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.auth_user
-    ADD CONSTRAINT auth_user_username_key UNIQUE (username);
+ALTER TABLE ONLY public.movies
+    ADD CONSTRAINT movies_pkey PRIMARY KEY (movie_id);
 
 
 --
