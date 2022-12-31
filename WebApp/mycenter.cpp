49eae9ac28d8a888333a4ec8acbd373efd67fa15
singularity <12184989+singularity-s0@users.noreply.github.com>
2022-12-31 15:31:08 +0800

    tickets ui

--- WebApp/mycenter.cpp
+++ WebApp/mycenter.cpp
@@ -3,6 +3,18 @@
 #include "rendering.h"
 
 extern bserv::db_relation_to_object orm_ticket;
+extern bserv::db_relation_to_object orm_screening;
+
+bserv::db_relation_to_object orm_full_ticket_info{
+    bserv::make_db_field<int>("ticket_id"),
+    bserv::make_db_field<std::string>("username"),
+    bserv::make_db_field<std::string>("showing_date"),
+    bserv::make_db_field<std::string>("time"),
+    bserv::make_db_field<std::string>("room"),
+    bserv::make_db_field<std::string>("seat_name"),
+    bserv::make_db_field<int>("price"),
+    bserv::make_db_field<bool>("refunded"),
+};
 
 std::nullopt_t redirect_to_mycenter(
     std::shared_ptr<bserv::db_connection> conn,
@@ -18,11 +30,13 @@ std::nullopt_t redirect_to_mycenter(
     int uid = user["id"].as_int64();
 
     bserv::db_transaction tx{conn};
-    bserv::db_result db_res = tx.exec("select * from tickets where tickets.user_id=?;", uid);
+    // Get full ticket info
+    bserv::db_result db_res = tx.exec("select tickets.ticket_id as ticket_id, users.username as username, screenings.showing_date as showing_date, screenings.time as time, screenings.room as room, seats.seat_name as seat_name, tickets.price as price, tickets.refunded as refunded from tickets, auth_user as users, screenings, seats where tickets.user_id=? and tickets.user_id=users.id and tickets.screening_id=screenings.screening_id and tickets.seat_id=seats.seat_id;", uid);
     lginfo << db_res.query();
-    auto tickets = orm_ticket.convert_to_vector(db_res);
+    auto tickets = orm_full_ticket_info.convert_to_vector(db_res);
     boost::json::array json_tickets;
     for (auto& ticket : tickets) {
+        ticket["time"] = ticket["showing_date"].as_string() + " " + ticket["time"].as_string();
         json_tickets.push_back(ticket);
     }
     context["tickets"] = json_tickets;
